f076e3a8a6807166caaf5a0d01c25d6d4d40655a
Chaitali <khadkechaitali0706@gmail.com>
2025-09-09 16:22:43 +0530

    I will update UI of apllication

--- app/src/main/java/com/sunbeaminfo/cameraapp/MainActivity.java
+++ app/src/main/java/com/sunbeaminfo/cameraapp/MainActivity.java
@@ -1,55 +1,74 @@
 package com.sunbeaminfo.cameraapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.sunbeaminfo.cameraapp.R;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-    Button click,rear,front;
-    Intent intent;
+    private ImageCapture imageCapture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        click=findViewById(R.id.click);
-        rear=findViewById(R.id.rear);
-        front=findViewById(R.id.front);
+        PreviewView previewView = findViewById(R.id.previewView);
+        Button captureButton = findViewById(R.id.captureButton);
 
-    }
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+                ProcessCameraProvider.getInstance(this);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
 
-    }
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+                imageCapture = new ImageCapture.Builder()
+                        .setTargetRotation(previewView.getDisplay().getRotation())
+                        .build();
 
-    }
-  public void click(View view) {
-       Intent intent = new Intent();
-       intent.setAction(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-       startActivity(intent);
-  }
-  public void front(View view) {
-        Intent intent = new Intent();
-        intent.setAction(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivity(intent);
-  }
-  public void rear(View view) {
-        Intent intent = new Intent();
-        intent.setAction(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivity(intent);
-  }
+                CameraSelector cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA;
+                cameraProvider.unbindAll();
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
 
-    @Override
-    public Intent getIntent() {
-        intent.putExtra(this.getString(R.string.app_name),this.getString(R.string.app_name));
-        return intent;
+        captureButton.setOnClickListener(v -> {
+            if (imageCapture == null) return;
+            File photoFile = new File(getExternalMediaDirs()[0],
+                    "IMG_" + System.currentTimeMillis() + ".jpg");
+
+            ImageCapture.OutputFileOptions outputOptions =
+                    new ImageCapture.OutputFileOptions.Builder(photoFile).build();
+
+            imageCapture.takePicture(outputOptions, ContextCompat.getMainExecutor(this),
+                    new ImageCapture.OnImageSavedCallback() {
+                        @Override
+                        public void onImageSaved(ImageCapture.OutputFileResults output) {
+                            // Optionally, update gallery and notify MediaStore
+                        }
+
+                        @Override
+                        public void onError(ImageCaptureException exception) {
+                            exception.printStackTrace();
+                        }
+                    });
+        });
     }
 }
